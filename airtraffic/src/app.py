30f9842352a4ee6b135ed92ae27e8aebbd1ea0d2
lily <nguyenhuong2671997@gmail.com>
2023-06-02 01:43:15 +0200

    done plotly sur streamlit

--- airtraffic/src/app.py
+++ airtraffic/src/app.py
@@ -1,19 +1,36 @@
 import streamlit as st
 import pandas as pd 
 
-import streamlit as st
-from PIL import Image
 
 
 st.title('Traffic Forecast')
-Home_airport = ('LGW', 'LIS')
-Paired_airport = ('FUE', 'AMS')
+Home_airport = ('LGW', 'LIS', "LYS", "NTE", "PNH", "POP", "SCL", "SSA")
+#Paired_airport = ('FUE', 'AMS')
+
+# Define the options for the second select box based on the selected value of the first select box
 # df = pd.read
 with st.sidebar:
     home_airport = st.selectbox(
         'Home Airport', Home_airport
     )
-
+    # Define the options for the second select box based on the selected value of the first select box
+    if home_airport == 'LGW':
+        Paired_airport = ("AMS", "BCN")
+    elif home_airport  == 'LIS':
+        Paired_airport = ("OPO", "ORY")
+    elif home_airport  == 'LYS':
+        Paired_airport = ("PIS",)
+    elif home_airport  == 'NTE':
+        Paired_airport = ("FUE",)  
+    elif home_airport  == 'PNH':
+        Paired_airport = ("NGB",) 
+    elif home_airport  == 'POP':
+        Paired_airport = ("JFK",)  
+    elif home_airport  == 'SCL':
+        Paired_airport = ("LHR",) 
+    elif home_airport  == 'SSA':
+        Paired_airport = ("GRU",) 
+    
     paired_airport = st.selectbox(
         'Paired Airport', Paired_airport
     )
@@ -26,10 +43,10 @@ st.write('paired airport select', paired_airport)
 st.write('Day select', nb_days)
 st.write('Date select', forecast_date)
 
-# Load the image from file
-image = Image.open("/Users/lilyhuong/Desktop/Amse mag3/semestre 2/Forecast air traffic/airtraffic/images/LGW_AMS.png")
 
 # Display the image using Streamlit
-st.image(image, caption='Image Caption', use_column_width=True)
+link_image = "/Users/lilyhuong/Desktop/Amse mag3/semestre 2/Forecast air traffic/airtraffic/images/"
+file_img = link_image + home_airport + "_" + paired_airport + ".png"
+st.image(file_img)
 
 # st.write(df.querry('home_airport = "{}"'.format(home_airport)).shape[0])
