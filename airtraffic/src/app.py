1a0cc5b542d19e3bfe8f6b0a8cb911569cfb0673
lily <nguyenhuong2671997@gmail.com>
2023-06-01 20:55:09 +0200

    test

--- airtraffic/src/app.py
+++ airtraffic/src/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd 
 st.title('Traffic Forecast')
-Home_airport = ('LGW', 'LIS', 'LYS')
-Paired_airport = ('FUE', 'AMS', 'ORY')
+Home_airport = ('LGW', 'LIS')
+Paired_airport = ('FUE', 'AMS')
 # df = pd.read
 with st.sidebar:
     home_airport = st.selectbox(
@@ -13,7 +13,7 @@ with st.sidebar:
         'Paired Airport', Paired_airport
     )
     forecast_date = st.date_input('Forecast Start Date')
-    nb_days = st.slider("Day of forecats", 7, 30, 1)
+    nb_days = st.slider("Day of forecats", 7, 40, 1)
     run_forecast = st.button("Forecast")
 
 st.write('Home airport select', home_airport)
