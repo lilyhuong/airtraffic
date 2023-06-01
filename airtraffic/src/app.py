c5b3e69d3b52cc1e9f615e1617f8833b9b7a23e9
lily <nguyenhuong2671997@gmail.com>
2023-06-02 00:43:48 +0200

    test

--- airtraffic/src/app.py
+++ airtraffic/src/app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd 
+
+import streamlit as st
+from PIL import Image
+
+
 st.title('Traffic Forecast')
 Home_airport = ('LGW', 'LIS')
 Paired_airport = ('FUE', 'AMS')
@@ -21,5 +26,10 @@ st.write('paired airport select', paired_airport)
 st.write('Day select', nb_days)
 st.write('Date select', forecast_date)
 
-st.image('/Users/lilyhuong/Desktop/Amse mag3/semestre 2/Forecast air traffic/airtraffic/images/LGW_AMS.png')
+# Load the image from file
+image = Image.open("/Users/lilyhuong/Desktop/Amse mag3/semestre 2/Forecast air traffic/airtraffic/images/LGW_AMS.png")
+
+# Display the image using Streamlit
+st.image(image, caption='Image Caption', use_column_width=True)
+
 # st.write(df.querry('home_airport = "{}"'.format(home_airport)).shape[0])
